41816d440616e68d50da3dbc65d8d61e679a372d
Andy Lulham <a.lulham@gmail.com>
2019-06-11 16:20:00 +0100

    Massively simplify by removing stats computation

--- run.sh
+++ run.sh
@@ -2,5 +2,23 @@
 
 set -e
 
-# ./run_stats.sh
-./run_dashboard.sh
+cd IATI-Dashboard
+rm -rf stats-calculated stats-blacklist
+
+# Fetch the necessary calculated stats
+./get_stats.sh
+./git.sh
+
+# setup the output directory
+git clone --branch gh-pages https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/andylolz/dashboard.git out
+cd out
+rm -rf *
+cp ../web/* .
+
+# push output to github
+git add --all
+git commit -m "auto"
+git push origin gh-pages
+cd ..
+
+cd ..
